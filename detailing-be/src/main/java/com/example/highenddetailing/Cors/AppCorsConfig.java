9a260ecac229fd10dff0833616e590127e4970bc
Jesse Bourassa <jessebourassa@Jesses-MacBook-Air.local>
2025-01-16 16:01:40 -0500

    qucik fix

--- detailing-be/src/main/java/com/example/highenddetailing/Cors/AppCorsConfig.java
+++ detailing-be/src/main/java/com/example/highenddetailing/Cors/AppCorsConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig {
+public class AppCorsConfig {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
