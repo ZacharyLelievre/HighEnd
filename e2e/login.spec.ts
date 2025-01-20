a9bf16cc5685bf99d078564c3fb6f692e225284f
ZacharyLelievre <lelievrezachary@gmail.com>
2025-01-20 02:28:58 -0500

    playwright test

---
+++ e2e/login.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('Login', async ({ page }) => {
+  await page.goto('https://highend-1.onrender.com/home');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByLabel('Email address').click();
+  await page.getByLabel('Email address').fill('admin@admin.com');
+  await page.getByLabel('Password').click();
+  await page.getByLabel('Password').fill('Admin33##');
+  await page.getByRole('button', { name: 'Continue', exact: true }).click();
+  await expect(page.locator('#basic-navbar-nav')).toContainText('Dashboard');
+});
---
+++ e2e/signup.spec.ts
@@ -0,0 +1,31 @@
+import { test, expect } from '@playwright/test';
+
+test('Signup', async ({ page }) => {
+  await page.goto('https://highend-1.onrender.com/home');
+  await page.getByRole('button', { name: 'Register' }).click();
+  await page.getByPlaceholder('First Name').click();
+  await page.getByPlaceholder('First Name').fill('z');
+  await page.getByPlaceholder('Last Name').click();
+  await page.getByPlaceholder('Last Name').fill('l');
+  await page.getByPlaceholder('Email').click();
+  await page.getByPlaceholder('Email').fill('leli@gmail.com');
+  await page.getByPlaceholder('Street Address').click();
+  await page.getByPlaceholder('Street Address').fill('f');
+  await page.getByPlaceholder('City').click();
+  await page.getByPlaceholder('City').fill('f');
+  await page.getByPlaceholder('Postal Code').click();
+  await page.getByPlaceholder('Postal Code').fill('f');
+  await page.getByPlaceholder('Street Address').click();
+  await page.getByPlaceholder('Street Address').fill('fak');
+  await page.getByPlaceholder('City').click();
+  await page.getByPlaceholder('City').fill('fak');
+  await page.getByPlaceholder('Postal Code').click();
+  await page.getByPlaceholder('Province').click();
+  await page.getByPlaceholder('Province').fill('q');
+  await page.getByPlaceholder('Country').click();
+  await page.getByPlaceholder('Country').fill('q');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await expect(page.locator('section')).toContainText('Log in');
+  await expect(page.locator('header')).toContainText('Sign Up to dev-vmtwqb6p6lr3if0d to continue to High End Detailling.');
+});
