a67698f6ccc41305c2bce45cfd5d100aa9eae5b3
TheUnnamedOne1 <jesse.bou@outlook.com>
2025-02-11 01:31:53 -0500

    test

--- e2e/test-5.spec.ts
+++ e2e/test-5.spec.ts
@@ -1,11 +1,11 @@
-import { test, expect } from "@playwright/test";
-
-test("test", async ({ page }) => {
-  await page.goto("https://highend-1.onrender.com/dashboard");
-  await page.getByRole("button", { name: "Login" }).click();
-  await page.getByLabel("Email address").click();
-  await page.getByLabel("Email address").fill("j");
-  await page.getByLabel("Password").click();
-  await page.getByLabel("Password").press("CapsLock");
-  await page.getByRole("button", { name: "Continue", exact: true }).click();
-});
+// import { test, expect } from "@playwright/test";
+//
+// test("test", async ({ page }) => {
+//   await page.goto("https://highend-1.onrender.com/dashboard");
+//   await page.getByRole("button", { name: "Login" }).click();
+//   await page.getByLabel("Email address").click();
+//   await page.getByLabel("Email address").fill("j");
+//   await page.getByLabel("Password").click();
+//   await page.getByLabel("Password").press("CapsLock");
+//   await page.getByRole("button", { name: "Continue", exact: true }).click();
+// });
